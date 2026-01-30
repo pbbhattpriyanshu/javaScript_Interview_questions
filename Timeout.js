6caeea7dba578754fb08716c593b1b2b8ef711ec
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-01-30 22:26:52 +0530

    `Update console logs in Timeout.js`

--- Timeout.js
+++ Timeout.js
@@ -1,7 +1,7 @@
-console.log("Start");
+console.log("Start of Script");
 
 setTimeout(() => {
   console.log("This is from the Task Oueue (setTimeout)");
 }, 0);
 
-console.log("End");
+console.log("End of Script");
