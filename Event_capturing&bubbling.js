c1b94296d566f43b271d653c408551c77f1ba13c
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-22 12:22:07 +0530

    docs: Add definitions for Event Capturing and Event Bubbling.

---
+++ Event_capturing&bubbling.js
@@ -0,0 +1,5 @@
+//Event Capturing
+// A propagation model where events are handled from outer elements to inner elements
+
+//Event Bubbling
+// A propagation model where events are handled from inner elements to outer elements
