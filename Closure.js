34498c0f9ddcde04db785b23a302f6d24c800cf8
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-22 12:45:08 +0530

    docs: Define and explain JavaScript closures.

--- Closure.js
+++ Closure.js
@@ -1,3 +1,7 @@
+//What is closure
+// A closure is a function that has access to its outer function's scope even after the outer function has returned.
+// In simple words, a closure is a function that remembers the environment in which it was created.
+
 function counter() {
   let count = 0;
 
