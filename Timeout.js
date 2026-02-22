61d436b176b25d4184b2035260170b30e38c611a
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-22 13:42:25 +0530

    feat: Add demonstrations of `clearTimeout` and `clearInterval` to manage timer execution.

--- Timeout.js
+++ Timeout.js
@@ -4,15 +4,18 @@
 
 //setTimeout
 
-console.log("Start of Script");
-setTimeout(() => {
+const timeId = setTimeout(() => {
   console.log("This is from the Task Oueue (setTimeout)");
 }, 1000);
 
-console.log("End of Script");
 
 //setInterval: It is used to execute a function after a certain time interval
 
-// setInterval(() => {
-//   console.log("This is from the Task Oueue (setInterval)");
-// }, 1000);
+const timeid = setInterval(() => {
+  console.log("This is from the Task Oueue (setInterval)");
+}, 1000);
+
+//clearInterval: It is used to clear the interval set by setInterval
+
+clearTimeout(timeId);
+clearInterval(timeid);
