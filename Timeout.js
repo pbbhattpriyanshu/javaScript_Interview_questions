cdefa9d79310c46820fa8629538a7b0a7c52c68a
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2026-02-22 13:18:45 +0530

    docs: Add explanations for `setTimeout` and `setInterval`, update `setTimeout` delay to 1000ms, and include a commented `setInterval` example.

--- Timeout.js
+++ Timeout.js
@@ -1,7 +1,18 @@
-console.log("Start of Script");
+//Why we use: setTimeout
+// setTimeout is used to delay the execution of a function
+// It is also used to execute a function after a certain time interval
 
+//setTimeout
+
+console.log("Start of Script");
 setTimeout(() => {
   console.log("This is from the Task Oueue (setTimeout)");
-}, 0);
+}, 1000);
 
 console.log("End of Script");
+
+//setInterval: It is used to execute a function after a certain time interval
+
+// setInterval(() => {
+//   console.log("This is from the Task Oueue (setInterval)");
+// }, 1000);
