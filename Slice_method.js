516ae1c318271455d08a2a4e3157259953be05bb
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-04-10 17:11:58 +0530

    Slice Method

---
+++ Slice_method.js
@@ -0,0 +1,7 @@
+//Slice Method
+//The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included). The original array will not be modified.
+
+let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango", "Pineapple", "Watermelon", "Guvava"];
+
+let sliced = fruits.slice(2,7)
+console.log(sliced);
